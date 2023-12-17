2787a77cddf1e7e68ce4d857cf09c160d3dbc096
justice chimobi <chimobi.justice@gmail.com>
2023-12-17 08:21:42 +0100

    updated cache to reload

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'https://estudy.onrender.com'],
+    'allowed_origins' => ['*', 'localhost:3000', 'https://estudy.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
